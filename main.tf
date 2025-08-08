211c87acca2838f15f5126025e1fe9e6057685bd
billgrant <bill.grant@hashicorp.com>
2025-08-08 12:12:33 -0400

    Added Server side encryption

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ terraform {
 resource "aws_s3_bucket" "simple" {
     region = var.aws_region
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "simple" {
+  bucket = aws_s3_bucket.simple.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
